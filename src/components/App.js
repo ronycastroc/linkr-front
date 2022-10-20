24c3be10fee29b36a616d1c8cfa9fd5b78200fda
Yan Amaral <yan@pop-os.localdomain>
2022-10-20 11:31:06 -0300

    feat: modal for post deletion

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import GlobalStyle from "../assets/style/GlobalStyle";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DeletePost from "./DeleteModal";
 
 export default function App() {
-   return (
-      <>
-         <GlobalStyle />
-         <h1>TESTE NPM START {/* APAGAR */}</h1>
-         <BrowserRouter>
-            <Routes>
-               <Route path="/" element={''} />
-            </Routes>
-         </BrowserRouter>
-      </>
-   );
-};
+  return (
+    <>
+      <GlobalStyle />
+      <h1>TESTE NPM START {/* APAGAR */}</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={""} />
+          <Route path="/delete" element={<DeletePost />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -11,10 +11,17 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
+    backgroundColor: "#333333",
+    borderRadius: "30px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    fontWeight: "700",
+    fontSize: "34px",
+    textAlign: "center",
   },
 };
-
-Modal.setAppElement("#root");
 
 export default function DeletePost() {
   let subtitle;
@@ -35,44 +42,32 @@ export default function DeletePost() {
   return (
     <div>
       <button onClick={openModal}>Open Modal</button>
-      <ModalStyle>
-        <Modal
-          isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Delete Post Modal"
-          shouldCloseOnOverlayClick={false}
-        >
-          <Confirmation>
-            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
-              Are you sure you want to delete this post?
-            </h2>
-          </Confirmation>
 
-          <Options>
-            <button onClick={closeModal}>No, go back</button>
-            <button onClick={closeModal}>Yes, delete it</button>
-          </Options>
-        </Modal>
-      </ModalStyle>
+      <Modal
+        appElement={document.getElementsByClassName("root") || undefined}
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Delete Post Modal"
+        shouldCloseOnOverlayClick={false}
+      >
+        <Confirmation>
+          <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
+            Are you sure you want to delete this post?
+          </h2>
+        </Confirmation>
+
+        <Options>
+          <BackButton onClick={closeModal}>No, go back</BackButton>
+          <ConfirmDeleteButton onClick={closeModal}>
+            Yes, delete it
+          </ConfirmDeleteButton>
+        </Options>
+      </Modal>
     </div>
   );
 }
-
-const ModalStyle = styled.div`
-  position: absolute;
-  top: 40px;
-  left: 40px;
-  right: 40px;
-  bottom: 40px;
-  background-color: #333333;
-  border-radius: 30px;
-  height: 150px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
 
 const Confirmation = styled.div`
   display: flex;
@@ -80,6 +75,8 @@ const Confirmation = styled.div`
   justify-content: center;
   width: 55%;
   text-align: center;
+  margin-top: 15px;
+  margin-bottom: 15px;
 `;
 
 const Options = styled.div`
@@ -87,4 +84,37 @@ const Options = styled.div`
   align-items: center;
   justify-content: center;
   width: 55%;
+`;
+
+const ConfirmDeleteButton = styled.div`
+  width: 135px;
+  height: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #1877f2;
+  border-radius: 5px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  margin-left: 20px;
+`;
+
+const BackButton = styled.div`
+  width: 135px;
+  height: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  border-radius: 5px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  color: #1877f2;
+  border: none;
+  cursor: pointer;
 `;
