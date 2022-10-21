fa95b666e9a017c58c510d847570b59f11417da8
Yan Amaral <yan@pop-os.localdomain>
2022-10-21 17:51:43 -0300

    feat: delete icon and confirmation modal

--- src/service/linkrService.js
+++ src/service/linkrService.js
@@ -5,43 +5,53 @@ import axios from "axios";
 const BASE_URL = "http://localhost:4000";
 
 function createHeaders() {
-    const auth = JSON.parse(localStorage.getItem("token"));
-    
-   
-    const config = {
-      headers: {
-        Authorization: `Bearer ${auth}`,
-      },
-    };
-  
-    return config;
+  const auth = JSON.parse(localStorage.getItem("token"));
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${auth}`,
+    },
   };
 
-  function postSignUp(body) {
-    const promise = axios.post(`${BASE_URL}/auth/sign-up`, body);
-    return promise;
-  };
-  
-  function postSignIn(body) {
-    const promise = axios.post(`${BASE_URL}/auth/sign-in`, body);
-    return promise;
-  };
-  function postPublication(body){
-    const config = createHeaders();
-    const promise = axios.post(`${BASE_URL}/timeline`, body,config);
-    return promise;
-  };
-  function getPublications(){
-    const config = createHeaders();
-    const promise = axios.get(`${BASE_URL}/timeline`,config);
-    return promise;
-  }
-  function getHashtagTrending () {
-    //const config = createHeaders();
-    const promise = axios.get(`${BASE_URL}/trending`);
-    return promise;
-  }
+  return config;
+}
 
-  export { postSignUp, postSignIn, postPublication ,getPublications,getHashtagTrending};
-  
+function postSignUp(body) {
+  const promise = axios.post(`${BASE_URL}/auth/sign-up`, body);
+  return promise;
+}
 
+function postSignIn(body) {
+  const promise = axios.post(`${BASE_URL}/auth/sign-in`, body);
+  return promise;
+}
+function postPublication(body) {
+  const config = createHeaders();
+  const promise = axios.post(`${BASE_URL}/timeline`, body, config);
+  return promise;
+}
+function getPublications() {
+  const config = createHeaders();
+  const promise = axios.get(`${BASE_URL}/timeline`, config);
+  return promise;
+}
+function getHashtagTrending() {
+  //const config = createHeaders();
+  const promise = axios.get(`${BASE_URL}/trending`);
+  return promise;
+}
+
+function deletePost(body) {
+  const config = createHeaders();
+  const promise = axios.delete(`${BASE_URL}/timeline`, body, config);
+  return promise;
+}
+
+export {
+  postSignUp,
+  postSignIn,
+  postPublication,
+  getPublications,
+  getHashtagTrending,
+  deletePost,
+};
