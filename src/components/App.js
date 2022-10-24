d682a9ab41acc16f2b7617fdaa512007d103e6f0
Victormarzo <victor.marzo.monteiro@gmail.com>
2022-10-24 09:04:53 -0300

    Private page funcionando

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ import SignUp from "./Pages/SignUp";
 import TimeLineExample from "./Pages/TimeLineExample";
 import Header from "./header/Header";
 import HashtagPage from "./hashtag/HashtagPage"
+import PrivatePage from "../PrivatePage.js";
 
 export default function App() {
   const [refresh,setRefresh]=useState(false) 
@@ -22,9 +23,9 @@ export default function App() {
             <Routes>
                <Route path="/" element={<SignIn />} />
                <Route path="/sign-up" element={<SignUp />} />
-               <Route path="/home" element={<Header/>} />
-               <Route path="/hashtag/:hashtag" element={<HashtagPage />} />  
-               <Route path="/timeline" element={<Timeline/>}/>
+               <Route path="/home" element={<PrivatePage><Header/></PrivatePage>} />
+               <Route path="/hashtag/:hashtag" element={<PrivatePage><HashtagPage /></PrivatePage>} />  
+               <Route path="/timeline" element={<PrivatePage><Timeline/></PrivatePage>}/>
                             
             </Routes>
             </UserContext.Provider>
